5c1d67f99a6acc8da1d3d7332ef85cda0436217b
Priya Gupta <priya.gupta3001@gmail.com>
2016-04-01 13:42:31 +0530

    Demo Commit

--- test/src/test/Test1.java
+++ test/src/test/Test1.java
@@ -2,8 +2,9 @@ package test;
 
 public class Test1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello");
 
 	}
 
